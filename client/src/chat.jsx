13f481f71494e71918c09319f8652d0d38e5fcc0
Rochelle Norman <rochelle@thisissoon.com>
2021-01-12 19:28:20 +0000

    added inline styles and state

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -1,14 +1,85 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Container, Row, Col, FormInput, Button } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+
+  return !data ? null : (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          {user === messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: "0.5em",
+                border: "2px solid #e5e6ea",
+                borderRadius: 25,
+                textAlign: "center",
+                fontSize: "18pt",
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? "#58bf56" : "#e5e6ea",
+              color: user === messageUser ? "white" : "black",
+              padding: "1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
 const Chat = () => {
-  return <div>I'm a chat window</div>;
+  const [state, stateSet] = React.useState({
+    user: "Jack",
+    content: "",
+  });
+  return (
+    <Container>
+      <Messages user={state.user} />
+    </Container>
+  );
 };
 
 export default () => (
